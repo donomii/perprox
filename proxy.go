824f642c83c31dfe36b8ea25dd00ac768760a2d9
donomii <donomii@gmail.com>
2021-04-06 21:51:30 +0200

    Fix data leak between threads, remove unused code

--- proxy.go
+++ proxy.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"bytes"
-	"crypto/tls"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -11,7 +9,6 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
 	"os"
 	"os/signal"
@@ -19,13 +16,21 @@ import (
 	"regexp"
 	"strings"
 	"sync"
-	"time"
 
 	//	"github.com/mattn/go-sqlite3"
 
-	"github.com/boltdb/bolt"
+	"github.com/donomii/goof"
 	"github.com/elazarl/goproxy"
 )
+
+var stripchatmodels map[string]string = map[string]string{}
+
+type AppendRequest struct {
+	Path string
+	Data []byte
+}
+
+var appendChan chan AppendRequest = make(chan AppendRequest, 1000)
 
 func orPanic(err error) {
 	if err != nil {
@@ -33,52 +38,13 @@ func orPanic(err error) {
 	}
 }
 
-var dbMutex = sync.Mutex{}
 var counterMutex = sync.Mutex{}
-var db *bolt.DB
 var counters = map[string]int{}
 
 func add1(s string) {
 	counterMutex.Lock()
 	defer counterMutex.Unlock()
 	counters[s] = counters[s] + 1
-}
-
-type FileStream struct {
-	path string
-	f    *os.File
-}
-
-func NewFileStream(path string) *FileStream {
-	return &FileStream{path, nil}
-}
-
-func (fs *FileStream) Write(b []byte) (nr int, err error) {
-	if fs.f == nil {
-		fs.f, err = os.Create(fs.path)
-		if err != nil {
-			return 0, err
-		}
-	}
-	return fs.f.Write(b)
-}
-
-func (fs *FileStream) Close() error {
-	//fmt.Println("Close", fs.path)
-	if fs.f == nil {
-		return errors.New("FileStream was never written into")
-	}
-	return fs.f.Close()
-}
-
-type Meta struct {
-	req      *http.Request
-	resp     *http.Response
-	err      error
-	t        time.Time
-	sess     int64
-	bodyPath string
-	from     string
 }
 
 func fprintf(nr *int64, err *error, w io.Writer, pat string, a ...interface{}) {
@@ -88,148 +54,6 @@ func fprintf(nr *int64, err *error, w io.Writer, pat string, a ...interface{}) {
 	var n int
 	n, *err = fmt.Fprintf(w, pat, a...)
 	*nr += int64(n)
-}
-
-func write(nr *int64, err *error, w io.Writer, b []byte) {
-	if *err != nil {
-		return
-	}
-	var n int
-	n, *err = w.Write(b)
-	*nr += int64(n)
-}
-
-func (m *Meta) WriteTo(w io.Writer) (nr int64, err error) {
-	if m.req != nil {
-		fprintf(&nr, &err, w, "Type: request\r\n")
-	} else if m.resp != nil {
-		fprintf(&nr, &err, w, "Type: response\r\n")
-	}
-	fprintf(&nr, &err, w, "ReceivedAt: %v\r\n", m.t)
-	fprintf(&nr, &err, w, "Session: %d\r\n", m.sess)
-	fprintf(&nr, &err, w, "From: %v\r\n", m.from)
-	if m.err != nil {
-		// note the empty response
-		fprintf(&nr, &err, w, "Error: %v\r\n\r\n\r\n\r\n", m.err)
-	} else if m.req != nil {
-		fprintf(&nr, &err, w, "\r\n")
-		buf, err2 := httputil.DumpRequest(m.req, false)
-		if err2 != nil {
-			return nr, err2
-		}
-		write(&nr, &err, w, buf)
-	} else if m.resp != nil {
-		fprintf(&nr, &err, w, "\r\n")
-		buf, err2 := httputil.DumpResponse(m.resp, false)
-		if err2 != nil {
-			return nr, err2
-		}
-		write(&nr, &err, w, buf)
-	}
-	return
-}
-
-type nopCloser struct {
-	*bytes.Buffer
-}
-
-func (*nopCloser) Close() error { return nil }
-
-// Response represents the response from an HTTP request.
-//
-
-type MyResponse struct {
-	Status     string // e.g. "200 OK"
-	StatusCode int    // e.g. 200
-	Proto      string // e.g. "HTTP/1.0"
-	ProtoMajor int    // e.g. 1
-	ProtoMinor int    // e.g. 0
-
-	// Header maps header keys to values.  If the response had multiple
-	// headers with the same key, they may be concatenated, with comma
-	// delimiters.  (Section 4.2 of RFC 2616 requires that multiple headers
-	// be semantically equivalent to a comma-delimited sequence.) Values
-	// duplicated by other fields in this struct (e.g., ContentLength) are
-	// omitted from Header.
-	//
-	// Keys in the map are canonicalized (see CanonicalHeaderKey).
-	Header http.Header
-
-	// Body represents the response body.
-	//
-	// The http Client and Transport guarantee that Body is always
-	// non-nil, even on responses without a body or responses with
-	// a zero-length body. It is the caller's responsibility to
-	// close Body.
-	//
-	// The Body is automatically dechunked if the server replied
-	// with a "chunked" Transfer-Encoding.
-	Body []byte
-
-	// ContentLength records the length of the associated content.  The
-	// value -1 indicates that the length is unknown.  Unless Request.Method
-	// is "HEAD", values >= 0 indicate that the given number of bytes may
-	// be read from Body.
-	ContentLength int64
-
-	// Contains transfer encodings from outer-most to inner-most. Value is
-	// nil, means that "identity" encoding is used.
-	TransferEncoding []string
-
-	// Close records whether the header directed that the connection be
-	// closed after reading Body.  The value is advice for clients: neither
-	// ReadResponse nor Response.Write ever closes a connection.
-	Close bool
-
-	// Uncompressed reports whether the response was sent compressed but
-	// was decompressed by the http package. When true, reading from
-	// Body yields the uncompressed content instead of the compressed
-	// content actually set from the server, ContentLength is set to -1,
-	// and the "Content-Length" and "Content-Encoding" fields are deleted
-	// from the responseHeader. To get the original response from
-	// the server, set Transport.DisableCompression to true.
-	Uncompressed bool
-
-	// Trailer maps trailer keys to values, in the same
-	// format as the header.
-	Trailer http.Header
-
-	// The Request that was sent to obtain this Response.
-	// Request's Body is nil (having already been consumed).
-	// This is only populated for Client requests.
-	Request *http.Request
-
-	// TLS contains information about the TLS connection on which the
-	// response was received. It is nil for unencrypted responses.
-	// The pointer is shared between responses and should not be
-	// modified.
-	TLS *tls.ConnectionState
-}
-
-type TeeReadCloser struct {
-	r io.Reader
-	w io.WriteCloser
-	c io.Closer
-}
-
-func NewTeeReadCloser(r io.ReadCloser, w io.WriteCloser) io.ReadCloser {
-	return &TeeReadCloser{io.TeeReader(r, w), w, r}
-}
-
-func (t *TeeReadCloser) Read(b []byte) (int, error) {
-	return t.r.Read(b)
-}
-
-func (t *TeeReadCloser) Close() error {
-	err1 := t.c.Close()
-	err2 := t.w.Close()
-	if err1 == nil && err2 == nil {
-		return nil
-	}
-	if err1 != nil {
-		return err2
-	}
-	return err1
 }
 
 type stoppableListener struct {
@@ -260,7 +84,147 @@ func (sc *stoppableConn) Close() error {
 	return sc.Conn.Close()
 }
 
+var appendHandleCache = map[string]*os.File{}
+
+func appendWorker() {
+	for req := range appendChan {
+		path := req.Path
+		path = strings.Replace(path, ":", "", -1)
+		path = strings.Replace(path, "\\", "/", -1)
+		path = strings.Replace(path, "//", "/", -1)
+
+		log.Printf("Appending to path %v\n", path)
+
+		f, ok := appendHandleCache[path]
+		var err error
+		if !ok {
+			dir := filepath.Dir(path)
+			os.MkdirAll(dir, 0600)
+			f, err = os.OpenFile(path, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+
+			if err != nil {
+				log.Println("Error opening file: ", err)
+			}
+			appendHandleCache[path] = f
+		}
+		go func(data []byte, path string, f *os.File) {
+			_, err = f.Write(data)
+			if err != nil {
+				log.Println("Failed to write to ", path, " because ", err)
+			}
+		}(req.Data, path, f)
+
+		//f.Close()
+	}
+}
+
+func responseHandlerFunc(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+	fmt.Printf("Response: %+v\n", resp)
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return resp
+	}
+	//fmt.Printf("Body: %v\n", string(data))
+
+	fmt.Printf("[%d] %s %s\n", resp.StatusCode, ctx.Req.Method, ctx.Req.URL)
+	u := ctx.Req.URL.String()
+	upath := ctx.Req.URL.Path
+
+	if upath == "" {
+		upath = upath + "/index.html"
+	}
+	if upath == "." {
+		upath = upath + "index.html"
+	}
+	if strings.HasSuffix(upath, "/") {
+		upath = upath + "index.html"
+	}
+	path := filepath.Clean(upath)
+	path = strings.Replace(path, "\\", "/", -1)
+
+	log.Println("Examining path ", path)
+
+	uobj, err := url.Parse(u)
+	if err != nil {
+		r := bytes.NewReader(data)
+		resp.Body = ioutil.NopCloser(r)
+		return resp
+	}
+
+	//https: //stripchat.com/api/front/models/36737951/viewers/-5783  //referrer
+	if strings.Contains(path, "api/front/models") {
+		referrer := resp.Request.Referer()
+		referrer = strings.TrimPrefix(referrer, `https://stripchat.com/`)
+		bits := strings.Split(referrer, "?")
+		modelName := bits[0]
+
+		re := regexp.MustCompile(`front/models/(.+)/apps`)
+
+		streamName := string(re.Find([]byte(path)))
+		streamName = strings.TrimPrefix(streamName, "front/models/")
+		streamName = strings.TrimSuffix(streamName, "/apps")
+		if streamName != "" {
+			stripchatmodels[streamName] = modelName
+			log.Println("mapped stream id ", streamName, " to model ", modelName)
+		}
+	} else if strings.Contains(path, "live-hls") && strings.HasSuffix(path, ".ts") {
+		log.Println("Found stream in ", path)
+		re := regexp.MustCompile(`amlst:(.+)-`)
+		streamName := string(re.Find([]byte(path)))
+		streamName = strings.TrimPrefix(streamName, "amlst:")
+		streamName = strings.TrimSuffix(streamName, "-sd-")
+		if err == nil {
+
+			path = "streams/" + streamName + ".ts"
+
+			appendChan <- AppendRequest{path, data}
+		} else {
+			log.Println("Error naming stream:", err)
+		}
+	} else if strings.Contains(path, "/hls/") && strings.HasSuffix(path, ".ts") {
+		log.Println("Found stream in ", path)
+		re := regexp.MustCompile(`hls/(.+).ts`)
+		streamName := string(re.Find([]byte(path)))
+		streamName = strings.TrimPrefix(streamName, "hls/")
+		streamName = strings.TrimSuffix(streamName, ".ts")
+		bits := strings.Split(streamName, "/")
+		streamName = bits[0]
+		modelName, ok := stripchatmodels[streamName]
+		if !ok {
+			modelName = streamName
+		}
+		if err == nil {
+			path = "streams/" + modelName + ".ts"
+			appendChan <- AppendRequest{path, data}
+		} else {
+			log.Println("Error naming stream:", err)
+		}
+	} else {
+		path = "rip/" + uobj.Hostname() + "/" + path
+		path = strings.Replace(path, ":", "", -1)
+		path = strings.Replace(path, "\\", "/", -1)
+		path = strings.Replace(path, "//", "/", -1)
+
+		dir := filepath.Dir(path)
+		filename := filepath.Base(path)
+		os.MkdirAll(dir, 0600)
+		if goof.Exists(path) && filename == "stream" {
+
+			appendChan <- AppendRequest{path, data}
+		} else {
+			go func() {
+				fmt.Printf("Saving to path %v\n", path)
+				//ioutil.WriteFile(path, data, 0600)
+			}()
+		}
+	}
+	r := bytes.NewReader(data)
+	resp.Body = ioutil.NopCloser(r)
+	return resp
+}
+
 func main() {
+	go appendWorker()
 	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
 	addr := flag.String("l", ":8080", "on which address should the proxy listen")
 	flag.Parse()
@@ -276,35 +240,7 @@ func main() {
 	}
 	var err error
 
-	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
-		fmt.Printf("Response: %+v\n", resp)
-		data, _ := ioutil.ReadAll(resp.Body)
-		//fmt.Printf("Body: %v\n", string(data))
-
-		fmt.Printf("[%d] %s %s\n", resp.StatusCode, ctx.Req.Method, ctx.Req.URL)
-		u := ctx.Req.URL.String()
-		upath := ctx.Req.URL.Path
-
-		if upath == "" {
-			upath = upath + "/index.html"
-		}
-		if upath == "." {
-			upath = upath + "index.html"
-		}
-		if strings.HasSuffix(upath, "/") {
-			upath = upath + "index.html"
-		}
-		path := filepath.Clean(upath)
-		uobj, _ := url.Parse(u)
-		path = "rip/" + uobj.Hostname() + "/" + path
-		fmt.Printf("%v\n", path)
-		dir := filepath.Dir(path)
-		os.MkdirAll(dir, 0600)
-		ioutil.WriteFile(path, data, 0600)
-		r := bytes.NewReader(data)
-		resp.Body = ioutil.NopCloser(r)
-		return resp
-	})
+	proxy.OnResponse().DoFunc(responseHandlerFunc)
 
 	l, err := net.Listen("tcp", *addr)
 	if err != nil {
@@ -323,10 +259,6 @@ func main() {
 		sl.Done()
 	}()
 
-	db, err = bolt.Open("webcache.bolt", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
 	//defer db.Close()
 	log.Println("Starting Proxy on", *addr)
 	http.Serve(sl, proxy)

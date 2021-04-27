e8c9ffd7a5ac9252768a3bf55015b76d561284eb
donomii <donomii@gmail.com>
2021-04-27 08:33:20 +0200

    Use TeeReader to save files, reducing lag

--- proxy.go
+++ proxy.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
@@ -16,6 +16,7 @@ import (
 	"regexp"
 	"strings"
 	"sync"
+	"syscall"
 
 	//	"github.com/mattn/go-sqlite3"
 
@@ -26,8 +27,8 @@ import (
 var stripchatmodels map[string]string = map[string]string{}
 
 type AppendRequest struct {
-	Path string
-	Data []byte
+	Path     string
+	SavePipe io.Reader
 }
 
 var appendChan chan AppendRequest = make(chan AppendRequest, 1000)
@@ -84,146 +85,229 @@ func (sc *stoppableConn) Close() error {
 	return sc.Conn.Close()
 }
 
-var appendHandleCache = map[string]*os.File{}
+var appendHandleCache = map[string]*bufio.Writer{}
+var appendChanCache = map[string]*chan AppendRequest{}
 
+func fileWriteWorker(fileChan chan AppendRequest, f *bufio.Writer) {
+	for req := range fileChan {
+		go io.Copy(f, req.SavePipe)
+	}
+}
+
+func normalisePath(path string) string {
+	path = strings.Replace(path, ":", "", -1)
+	path = strings.Replace(path, "\\", "/", -1)
+	path = strings.Replace(path, "//", "/", -1)
+
+	path = strings.Replace(path, "=", "", -1)
+	path = strings.Replace(path, ".com", "", -1)
+	return path
+}
 func appendWorker() {
 	for req := range appendChan {
-		path := req.Path
-		path = strings.Replace(path, ":", "", -1)
-		path = strings.Replace(path, "\\", "/", -1)
-		path = strings.Replace(path, "//", "/", -1)
+		path := normalisePath(req.Path)
 
 		log.Printf("Appending to path %v\n", path)
-
-		f, ok := appendHandleCache[path]
-		var err error
-		if !ok {
+		var ch chan AppendRequest
+		chp, ok := appendChanCache[path]
+		if ok {
+			ch := *chp
+			ch <- req
+		} else {
 			dir := filepath.Dir(path)
 			os.MkdirAll(dir, 0600)
-			f, err = os.OpenFile(path, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+			fh, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 
 			if err != nil {
 				log.Println("Error opening file: ", err)
+			} else {
+				f := bufio.NewWriterSize(fh, 100000)
+				appendHandleCache[path] = f
+				ch = make(chan AppendRequest, 10000)
+				appendChanCache[path] = &ch
+				go fileWriteWorker(ch, f)
+				ch <- req
+				log.Println("Created new writepipe for", path)
 			}
-			appendHandleCache[path] = f
 		}
-		go func(data []byte, path string, f *os.File) {
-			_, err = f.Write(data)
-			if err != nil {
-				log.Println("Failed to write to ", path, " because ", err)
-			}
-		}(req.Data, path, f)
 
 		//f.Close()
 	}
 }
 
-func responseHandlerFunc(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
-	fmt.Printf("Response: %+v\n", resp)
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return resp
-	}
-	//fmt.Printf("Body: %v\n", string(data))
+func SendToAppend(req AppendRequest) {
+	path := normalisePath(req.Path)
 
-	fmt.Printf("[%d] %s %s\n", resp.StatusCode, ctx.Req.Method, ctx.Req.URL)
-	u := ctx.Req.URL.String()
-	upath := ctx.Req.URL.Path
-
-	if upath == "" {
-		upath = upath + "/index.html"
-	}
-	if upath == "." {
-		upath = upath + "index.html"
-	}
-	if strings.HasSuffix(upath, "/") {
-		upath = upath + "index.html"
-	}
-	path := filepath.Clean(upath)
-	path = strings.Replace(path, "\\", "/", -1)
-
-	log.Println("Examining path ", path)
-
-	uobj, err := url.Parse(u)
-	if err != nil {
-		r := bytes.NewReader(data)
-		resp.Body = ioutil.NopCloser(r)
-		return resp
-	}
-
-	//https: //stripchat.com/api/front/models/36737951/viewers/-5783  //referrer
-	if strings.Contains(path, "api/front/models") {
-		referrer := resp.Request.Referer()
-		referrer = strings.TrimPrefix(referrer, `https://stripchat.com/`)
-		bits := strings.Split(referrer, "?")
-		modelName := bits[0]
-
-		re := regexp.MustCompile(`front/models/(.+)/apps`)
-
-		streamName := string(re.Find([]byte(path)))
-		streamName = strings.TrimPrefix(streamName, "front/models/")
-		streamName = strings.TrimSuffix(streamName, "/apps")
-		if streamName != "" {
-			stripchatmodels[streamName] = modelName
-			log.Println("mapped stream id ", streamName, " to model ", modelName)
-		}
-	} else if strings.Contains(path, "live-hls") && strings.HasSuffix(path, ".ts") {
-		log.Println("Found stream in ", path)
-		re := regexp.MustCompile(`amlst:(.+)-`)
-		streamName := string(re.Find([]byte(path)))
-		streamName = strings.TrimPrefix(streamName, "amlst:")
-		streamName = strings.TrimSuffix(streamName, "-sd-")
-		if err == nil {
-
-			path = "streams/" + streamName + ".ts"
-
-			appendChan <- AppendRequest{path, data}
-		} else {
-			log.Println("Error naming stream:", err)
-		}
-	} else if strings.Contains(path, "/hls/") && strings.HasSuffix(path, ".ts") {
-		log.Println("Found stream in ", path)
-		re := regexp.MustCompile(`hls/(.+).ts`)
-		streamName := string(re.Find([]byte(path)))
-		streamName = strings.TrimPrefix(streamName, "hls/")
-		streamName = strings.TrimSuffix(streamName, ".ts")
-		bits := strings.Split(streamName, "/")
-		streamName = bits[0]
-		modelName, ok := stripchatmodels[streamName]
-		if !ok {
-			modelName = streamName
-		}
-		if err == nil {
-			path = "streams/" + modelName + ".ts"
-			appendChan <- AppendRequest{path, data}
-		} else {
-			log.Println("Error naming stream:", err)
-		}
+	chp, ok := appendChanCache[path]
+	if ok {
+		//log.Println("Found cached write pipe for", path)
+		ch := *chp
+		ch <- req
 	} else {
-		path = "rip/" + uobj.Hostname() + "/" + path
-		path = strings.Replace(path, ":", "", -1)
-		path = strings.Replace(path, "\\", "/", -1)
-		path = strings.Replace(path, "//", "/", -1)
-
-		dir := filepath.Dir(path)
-		filename := filepath.Base(path)
-		os.MkdirAll(dir, 0600)
-		if goof.Exists(path) && filename == "stream" {
-
-			appendChan <- AppendRequest{path, data}
-		} else {
-			go func() {
-				fmt.Printf("Saving to path %v\n", path)
-				//ioutil.WriteFile(path, data, 0600)
-			}()
-		}
+		log.Println("Creating new writepipe for", path)
+		appendChan <- req
 	}
-	r := bytes.NewReader(data)
-	resp.Body = ioutil.NopCloser(r)
+}
+
+type TeeReadCloser struct {
+	r io.Reader
+	w io.WriteCloser
+	c io.Closer
+}
+
+func NewTeeReadCloser(r io.ReadCloser, w io.WriteCloser) io.ReadCloser {
+	return &TeeReadCloser{io.TeeReader(r, w), w, r}
+}
+
+func (t *TeeReadCloser) Read(b []byte) (int, error) {
+	return t.r.Read(b)
+}
+
+func (t *TeeReadCloser) Close() error {
+	err1 := t.c.Close()
+	err2 := t.w.Close()
+	if err1 == nil && err2 == nil {
+		return nil
+	}
+	if err1 != nil {
+		return err2
+	}
+	return err1
+}
+
+func responseHandlerFunc(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+	//log.Printf("Response: %+v\n", resp)
+	bufIn := bufio.NewReader(resp.Body)
+	savePipe, savePipew := io.Pipe()
+	respData := NewTeeReadCloser(ioutil.NopCloser(bufIn), savePipew)
+	go func() {
+		//fmt.Printf("Body: %v\n", string(data))
+
+		fmt.Printf("[%d] %s %s\n", resp.StatusCode, ctx.Req.Method, ctx.Req.URL)
+		u := ctx.Req.URL.String()
+		upath := ctx.Req.URL.Path
+
+		if upath == "" {
+			upath = upath + "/index.html"
+		}
+		if upath == "." {
+			upath = upath + "index.html"
+		}
+		if strings.HasSuffix(upath, "/") {
+			upath = upath + "index.html"
+		}
+		path := filepath.Clean(upath)
+		path = strings.Replace(path, "\\", "/", -1)
+
+		//log.Println("Examining path ", path)
+
+		uobj, err := url.Parse(u)
+		if err != nil {
+			return
+		}
+
+		//https: //stripchat.com/api/front/models/36737951/viewers/-5783  //referrer
+		if strings.Contains(path, "api/front/models") {
+			log.Println("Stripchat url", path)
+			referrer := resp.Request.Referer()
+			referrer = strings.TrimPrefix(referrer, `https://stripchat.com/`)
+			bits := strings.Split(referrer, "?")
+			modelName := bits[0]
+
+			re := regexp.MustCompile(`front/models/(.+)/apps`)
+
+			streamName := string(re.Find([]byte(path)))
+			streamName = strings.TrimPrefix(streamName, "front/models/")
+			streamName = strings.TrimSuffix(streamName, "/apps")
+			if streamName != "" {
+				stripchatmodels[streamName] = modelName
+				log.Println("mapped stream id ", streamName, " to model ", modelName)
+			}
+		} else if strings.Contains(path, "live-hls") && strings.HasSuffix(path, ".ts") {
+			log.Println("Found chaturbate in ", path)
+			re := regexp.MustCompile(`amlst:(.+)-`)
+			streamName := string(re.Find([]byte(path)))
+			streamName = strings.TrimPrefix(streamName, "amlst:")
+			streamName = strings.TrimSuffix(streamName, "-sd-")
+			if err == nil {
+
+				path = "streams/" + streamName + ".ts"
+
+				SendToAppend(AppendRequest{path, savePipe})
+			} else {
+				log.Println("Error naming stream:", err)
+			}
+		} else if strings.Contains(path, "/hls/") && strings.HasSuffix(path, ".ts") {
+			log.Println("Found stripchat  in ", path)
+			re := regexp.MustCompile(`hls/(.+).ts`)
+			streamName := string(re.Find([]byte(path)))
+			streamName = strings.TrimPrefix(streamName, "hls/")
+			streamName = strings.TrimSuffix(streamName, ".ts")
+			bits := strings.Split(streamName, "/")
+			streamName = bits[0]
+			modelName, ok := stripchatmodels[streamName]
+			if !ok {
+				modelName = streamName
+			}
+			if err == nil {
+				path = "streams/" + modelName + ".ts"
+				SendToAppend(AppendRequest{path, savePipe})
+			} else {
+				log.Println("Error naming stream:", err)
+			}
+		} else {
+			path = "rip/" + uobj.Hostname() + "/" + path
+			path = strings.Replace(path, ":", "", -1)
+			path = strings.Replace(path, "\\", "/", -1)
+			path = strings.Replace(path, "//", "/", -1)
+
+			dir := filepath.Dir(path)
+			filename := filepath.Base(path)
+			os.MkdirAll(dir, 0600)
+			if goof.Exists(path) && filename == "stream" {
+				//SendToAppend(AppendRequest{path, savePipe})
+				go io.CopyBuffer(ioutil.Discard, savePipe, nil)
+			} else {
+				go func() {
+					path := normalisePath(path)
+					log.Printf("Saving whole file to path %v\n", path)
+					dir := filepath.Dir(path)
+					//log.Printf("makedir %v\n", dir)
+					os.MkdirAll(dir, 0600)
+					f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+					if err != nil {
+						log.Println(err, path)
+						go io.CopyBuffer(ioutil.Discard, savePipe, nil)
+						return
+					}
+					defer f.Close()
+					io.CopyBuffer(f, savePipe, nil)
+					f.Close()
+					savePipe.Close()
+				}()
+			}
+		}
+	}()
+	resp.Body = ioutil.NopCloser(respData)
 	return resp
 }
 
+func cleanup() {
+	for _, f := range appendHandleCache {
+		f.Flush()
+	}
+}
+
 func main() {
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		cleanup()
+		os.Exit(1)
+	}()
+
 	go appendWorker()
 	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
 	addr := flag.String("l", ":8080", "on which address should the proxy listen")
